ccf285bf4ea31b4fa1a59aa4677b022e67124349
geo <numiefriglapicioare@gmail.com>
2020-08-10 00:38:08 +0100

    add interview

---
+++ kodland/module1/interview.py
@@ -0,0 +1,30 @@
+name = input("what's ur name?")
+surname = input("what's ur surname?")
+print("Welcome to Coco Travel, ", name, surname)
+print()
+country = input("what's ur  fave country to visit?")
+transport = input("what's your fave way to get there? ")
+company = input("who would you like to travel  with?")
+print("We'd love to book ur next holiday to ", country, " and take you there by ", transport, " with your ", company)
+print()
+print("how much would u ideally be spending on ur visit to ", country, " my lovely ", name, "?")
+budget = input()
+time = input("How long would u like to stay there for?")
+print("I'm sure we will find u a perfect holiday for ", time, " and for the price of ", budget, "in the amazing ",
+      country, "!")
+
+# zodiak = input("What is your Zodiac ")
+# age = input("what is your age")
+# name = input("What is ur name ")
+# story = input(
+# "Tell a short story about your character in 3 sentences")
+
+# print("Hi! My name is " + name +"and I am" + zodiak + ". I am " + age + "years old.")
+
+# print(story)
+# What is your Zodiac   Taurus
+# what is your age  11
+# What is ur name   Georgiana
+# Tell a short story about your character in 3 sentences  We wanted a trampoline for  six yrs and now we have it.The only thing is that we have some REEAALLY ANNOYING neighbours who always want to play in our garden . But they're moving soon so that's  ok.
+# Hi! My name is Georgianaand I amTaurus. I am 11years old.
+# We wanted a trampoline for  six yrs and now we have it.The only thing is that we have some REEAALLY ANNOYING neighbours who always want to play in our garden . But they're moving soon so that's  ok.#
